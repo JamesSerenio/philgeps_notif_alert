3e6651c95e85a4d12dd09f861fed98ec717fc32a
JamesSerenio <sereniojames363@gmail.com>
2026-07-27 10:34:28 +0800

    added some code

--- lib/pdf_editor/services/pdf_service.dart
+++ lib/pdf_editor/services/pdf_service.dart
@@ -559,19 +559,19 @@ class PdfService {
     // punctuation remain untouched.
     graphics.drawRectangle(
       brush: whiteBrush,
-      bounds: const Rect.fromLTWH(210, 21, 270, 42),
+      bounds: const Rect.fromLTWH(210, 159, 270, 42),
     );
     graphics.drawString(
       purchaseOrderNumber,
       valueFont,
       brush: redBrush,
-      bounds: const Rect.fromLTWH(216, 25, 100, 18),
+      bounds: const Rect.fromLTWH(216, 163, 100, 18),
     );
     graphics.drawString(
       date,
       valueFont,
       brush: blackBrush,
-      bounds: const Rect.fromLTWH(216, 41, 220, 18),
+      bounds: const Rect.fromLTWH(216, 179, 220, 18),
     );
   }
 }
